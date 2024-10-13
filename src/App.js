f65d541865aa27b54dfde3b4c642335520321f77
zuannn <zuanrachmawan234@gmail.com>
2024-10-13 22:05:03 +0700

    addshopcategory

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path = "/" element = {<Shop />} />
-          <Route path = "/mens" element = {<ShopCategory banner = {mens} category = "mens" />} />
-          <Route path = "/womens" element = {<ShopCategory banner = {women} category = "womens" />} />
-          <Route path = "/kids" element = {<ShopCategory banner = {kids} category = "kids" />} />
+          <Route path = "/mens" element = {<ShopCategory banner = {mens} category = "men" />} />
+          <Route path = "/womens" element = {<ShopCategory banner = {women} category = "women" />} />
+          <Route path = "/kids" element = {<ShopCategory banner = {kids} category = "kid" />} />
           <Route path = "/product" element = {<Produck />} />
           <Route path = ":productId" element = {<Produck />} />
           <Route />
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,41 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import dropdown from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/item/Item";
 import './style/ShopCategory.css'
+import '../Components/Assets/all_product'
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
+
   return (
-    <div className="shop-category">
-      <img src={props.banner} alt="" />
-      <div className="index-short">
+    <div className = "shop-category">
+      <img src = {props.banner} alt = "" className="shop-banner"/>
+      <div className = "index-short">
         <p>
-          <span>showing1-12</span>out of 36 product
+          <span>Showing1-12</span>out of 36 product
         </p>
+      <div className = "short">
+        short by <img src = {dropdown} alt="" />
       </div>
-      <div className="short">
-        short by <img src={dropdown} alt="" />
       </div>
-      <div className="shop-category-product">
-        {all_product.map((item, i) => {
+      <div className = "shopcategory-product">
+        {all_product.map((item,i)=>{  
           if (props.category === item.category) {
-            return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
+            return <Item key = {i}
+            name = {item.name}
+            image = {item.image}
+            new_price = {item.new_price}
+            old_price = {item.old_price}
+            />
           }
-          else {
+          else{
             return null
           }
         })}
+         
       </div>
+      <div className="explore">Explore More</div>
     </div>
   );
 };
