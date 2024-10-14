70ed245af776953bb81168e5a35ced9754bf1972
zuannn <zuanrachmawan234@gmail.com>
2024-10-14 23:28:01 +0700

    asd

--- src/Components/Descrirption/Description.jsx
+++ src/Components/Descrirption/Description.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Description = () => {
   return (
     <div className='description'>
+        <div className="description-navigator">
+            <div className="description-nav-box">description</div>
+            <div className="description-nav-fade">review(122)</div>
+        </div>
       
     </div>
   )
